d2b73243b0e1123981d1fa55a62b7b2a3a3a7341
Fabricio Augusto <fabricionaweb@gmail.com>
2020-02-28 00:20:12 +0100

    Moved index.css to css-modules

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App/App'
 import * as serviceWorker from './serviceWorker'
-import './styles/index.css'
+import './styles/index.module.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
