e7cd8c7a51e39ff23540f1a1f96ffe9d84cb2fed
Fabricio Augusto <fabricionaweb@gmail.com>
2020-03-03 23:00:55 +0100

    Styles for heading and card

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -4,9 +4,16 @@ import styles from './Header.module.css'
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Dashboard</h1>
-      </div>
+      <h1 className={styles.title}>Dashboard</h1>
+
+      <dl className={styles.card}>
+        <dt>Available Balance</dt>
+        <dd className={styles.amount}>
+          <sup>€</sup>2500,53
+        </dd>
+        <dt hidden>Currency</dt>
+        <dd className={styles.currency}>EUR</dd>
+      </dl>
     </header>
   )
 }
